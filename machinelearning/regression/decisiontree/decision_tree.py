8489b3dc51e8bf872131d7ae74dc5b51ce798f21
Szymon Stefański <szymonstefanski1@gmail.com>
2026-05-05 00:03:22 +0200

    feat(docs): update wrong regressor from RandomForest to DecisionTree

--- machinelearning/regression/decisiontree/decision_tree.py
+++ machinelearning/regression/decisiontree/decision_tree.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor
 
 
 dataset = pd.read_csv('Position_Salaries.csv')
@@ -9,7 +9,7 @@ X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
 
-regressor = RandomForestRegressor(n_estimators=100, random_state=0)
+regressor = DecisionTreeRegressor(random_state=0)
 regressor.fit(X, y)
 
 
